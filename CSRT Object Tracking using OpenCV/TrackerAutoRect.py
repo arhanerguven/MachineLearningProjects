40441fb9e4ffcfc2fed4bec5b1b4e6d60749fc67
Arhan Bartu Ergüven <117912299+arhanerguven@users.noreply.github.com>
2023-07-25 13:12:38 +0300

    Update TrackerAutoRect.py

--- CSRT Object Tracking using OpenCV/TrackerAutoRect.py
+++ CSRT Object Tracking using OpenCV/TrackerAutoRect.py
@@ -30,7 +30,7 @@ ok, frame = video.read()
 print(frame.shape)
 
 if not ok:
-    print('Cannot read video file')
+    print('Video cannot be read')
     sys.exit()
 
 cv2.namedWindow('Showing')
@@ -54,35 +54,30 @@ while second_quit:
     if not ok:
         break
 
-    # Start timer
+ 
     timer = cv2.getTickCount()
-
-    # Update tracker
     ok, roi = tracker.update(frame)
     print(roi)
 
-    # Calculate Frames per second (FPS)
+   
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
     if ok:
         p1 = (int(roi[2]), int(roi[3]))
         p2 = (int(roi[0] + roi[2]), int(roi[1] + roi[3]))
         cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
     else:
-        # Tracking failure
-        cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+        cv2.putText(frame, "Tracking failure", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
-    # Display tracker type on frame
+   
     cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
-    # Display FPS on frame
     cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
-    # Display result
     cv2.imshow('Showing', frame)
 
-    # Exit if ESC pressed
-    k = cv2.waitKey(1) & 0xff
-    if k == 27: break
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
 
 while True:
     ok, frame = video.read()
@@ -92,21 +87,4 @@ while True:
     key = cv2.waitKey(20)
     if key == 27:
         break
-'''
-    if ok:
-        width = int(abs(roi[2]-roi[0])/2)
-        length = int(abs(roi[3]-roi[1])/2)
-        constant = 50
-        if roi[2] > roi[0]:
-            centerx = roi[0] + width
-        else:
-            centerx = roi[2] + width
 
-        if roi[3] > roi[1]:
-            centery = roi[1] + length
-        else:
-            centery = roi[3] + length
-
-        p1 = (int(centerx - width),int(centery-length))
-        p2 = (int(centerx + width),int(centery +length))
-'''
